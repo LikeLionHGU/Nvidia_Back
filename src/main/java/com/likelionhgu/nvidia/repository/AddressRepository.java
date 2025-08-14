dfd1be206c54fff43fecbbf4f0342d6fafa2f000
zionkim <22100130@handong.ac.kr>
2025-08-14 21:56:48 +0900

    repository

---
+++ src/main/java/com/likelionhgu/nvidia/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.likelionhgu.nvidia.repository;
+
+import com.likelionhgu.nvidia.domain.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
